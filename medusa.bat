74ee914ae2a066be81928a027d18fc8855391c6c
damon-kwok <damon-kwok@outlook.com>
2016-12-25 16:28:14 +0800

    update module

--- medusa.bat
+++ medusa.bat
@@ -108,7 +108,7 @@ rem rm -f .git/index.lock
 git reset
 git add *
 git status
-git commit -m "update module"
+git commit -m "%msg%"
 git push -u origin master
 goto:eof
 
@@ -116,7 +116,7 @@ goto:eof
 git reset
 git add *
 git status
-git commit -m "update app"
+git commit -m "update module"
 git push -u origin master
 goto:eof
 
@@ -185,7 +185,7 @@ echo    5) unzipapp
 echo    r) return
 set /p c=please input your choice:
 
-if /i "%c%"=="1" call:push
+if /i "%c%"=="1" call:push-a
 if /i "%c%"=="2" call:getapp
 if /i "%c%"=="3" call:pushapp
 if /i "%c%"=="4" call:zipapp
