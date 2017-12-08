18224f448ddbe3d0210fa0d79d1b131ea76889a1
damon-kwok <563066990@qq.com>
2017-12-08 21:06:55 +0800

    mod-cc

--- emacs-config/modules/mod-cc.el
+++ emacs-config/modules/mod-cc.el
@@ -200,13 +200,13 @@
 ;; `create-or-open-cmake-file'
 (defun m-search-file (filename &optional path) 
   (let ((from (if path path (m-buf-dirpath)))) 
-    (message (concat "check:" from)) 
-    (if (file-exists-p (concat from filename)) 
-	(progn (message from) from) 
-      (progn 
-	(setq parent (m-parent-dirpath from)) 
-	(if (or (eq parent nil) 
-		(string= parent "/")) nil (m-search-file filename parent))))))
+	(message (concat "check:" from)) 
+	(if (file-exists-p (concat from filename)) 
+		(progn (message from) from) 
+	  (progn 
+		(setq parent (m-parent-dirpath from)) 
+		(if (or (eq parent nil) 
+				(string= parent "/")) nil (m-search-file filename parent))))))
 
 (defun m-smart-find-file (filename &optional create) 
   " create cmake file with current directory!" 
@@ -214,50 +214,50 @@
   (setq dir (m-buf-dirpath)) 
   (setq cmake-dir (m-search-file filename dir)) 
   (if (eq cmake-dir nil) 
-      (if create (find-file filename)) 
-    (find-file (concat cmake-dir filename))))
+	  (if create (find-file filename)) 
+	(find-file (concat cmake-dir filename))))
 
 ;; switch 'cmake buffer' and 'code buffer'
 (defun m-open-or-close-cmakefile () 
   (interactive) 
   (if (or (eq major-mode 'c-mode) 
-	  (eq major-mode 'c++-mode)) 
-      (m-smart-find-file "CMakeLists.txt" t) 
-    (if (eq major-mode 'cmake-mode) ;;(if (equal buffer-name "CMakeLists.txt")
-	(kill-this-buffer))))
+		  (eq major-mode 'c++-mode)) 
+	  (m-smart-find-file "CMakeLists.txt" t) 
+	(if (eq major-mode 'cmake-mode) ;;(if (equal buffer-name "CMakeLists.txt")
+		(kill-this-buffer))))
 
 ;; switch 'package-xml buffer' and 'code buffer'
 (defun m-open-or-close-packagexml () 
   (interactive) 
   (if (or (eq major-mode 'c-mode) 
-	  (eq major-mode 'c++-mode)) 
-      (m-smart-find-file "package.xml") 
-    (if (eq major-mode 'nxml-mode) 
-	(kill-this-buffer))))
+		  (eq major-mode 'c++-mode)) 
+	  (m-smart-find-file "package.xml") 
+	(if (eq major-mode 'nxml-mode) 
+		(kill-this-buffer))))
 
 (defun find-cc-file (dir basename ext) 
   (let ((filename (concat dir "/" basename "." ext))) 
-    (if (file-exists-p filename) 
-	(find-file filename)
+	(if (file-exists-p filename) 
+		(find-file filename)
       ;; (progn (message "not found:%s" filename) nil)
-      nil				;
+      nil								;
       )))
 
 (defun check-header (dir basename) 
   (cond ((find-cc-file dir basename "h") t) 
-	((find-cc-file dir basename "H") t) 
-	((find-cc-file dir basename "hh") t) 
-	((find-cc-file dir basename "hpp") t) 
-	((find-cc-file dir basename "h++") t) 
-	((find-cc-file dir basename "hxx") t)))
+		((find-cc-file dir basename "H") t) 
+		((find-cc-file dir basename "hh") t) 
+		((find-cc-file dir basename "hpp") t) 
+		((find-cc-file dir basename "h++") t) 
+		((find-cc-file dir basename "hxx") t)))
 
 (defun check-source (dir basename) 
   (cond ((find-cc-file dir basename "c") t) 
-	((find-cc-file dir basename "C") t) 
-	((find-cc-file dir basename "cc") t) 
-	((find-cc-file dir basename "cpp") t) 
-	((find-cc-file dir basename "c++") t) 
-	((find-cc-file dir basename "cxx") t)))
+		((find-cc-file dir basename "C") t) 
+		((find-cc-file dir basename "cc") t) 
+		((find-cc-file dir basename "cpp") t) 
+		((find-cc-file dir basename "c++") t) 
+		((find-cc-file dir basename "cxx") t)))
 
 (defun m-switch-cc-source-and-header () 
   (interactive) 
@@ -271,19 +271,19 @@
   (setq dirs ()) 
   (add-to-list 'dirs dir0) 
   (dolist (postfix postfixes) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) postfix)) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) dir0-name)) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) postfix "/" dir0-name)) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" postfix)) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" dir0-name)) 
-    (add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" postfix "/" dir0-name))
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) postfix)) 
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) dir0-name)) 
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) postfix "/" dir0-name)) 
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" postfix)) 
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" dir0-name)) 
+	(add-to-list 'dirs (concat (m-parent-dirpath dir0) "../" postfix "/" dir0-name))
     ;;
     ) 
   (if (s-contains? "h" extname) 
-      (dolist (dir dirs) 
-	(check-source dir basename)) 
-    (dolist (dir dirs) 
-      (check-header dir basename))))
+	  (dolist (dir dirs) 
+		(check-source dir basename)) 
+	(dolist (dir dirs) 
+	  (check-header dir basename))))
 
 (require 'cmake-mode)
 (define-key c-mode-map [f6] 'm-open-or-close-cmakefile)
@@ -296,9 +296,10 @@
 (define-key c-mode-map [f12] 'm-switch-cc-source-and-header)
 (define-key c++-mode-map [f12] 'm-switch-cc-source-and-header)
 
-(define-key c++-mode-map [f5] '(lambda ()
-								 (interactive)
-								 (m-run-command "/home/damon/catkin_ws/bin/build_adsim")))
+(define-key c++-mode-map [f5] 
+  '(lambda () 
+	 (interactive) 
+	 (m-run-command "/home/damon/catkin_ws/bin/build_adsim")))
 
 (require 'nxml-mode)
 (define-key nxml-mode-map [f7] 'm-open-or-close-packagexml)
@@ -322,9 +323,7 @@
 (define-key c++-mode-map (kbd "C-M-\\")  'clang-format-region)
 (define-key objc-mode-map (kbd "C-M-\\")  'clang-format-region)
 
-(setq-default c-basic-offset 4
-              tab-width 4
-              indent-tabs-mode t)
+(setq-default c-basic-offset 4 tab-width 4 indent-tabs-mode t)
 (setq c-default-style "linux")
 ;; (package-require 'google-c-style)
 ;; (require 'google-c-style)
@@ -346,19 +345,63 @@
   (setq cmake-dir (m-search-file "CMakeLists.txt" dir)) 
   (setq index-dir (concat cmake-dir "rtags_indexes")) 
   (if (eq cmake-dir nil) 
-      (message "not found 'CMakeLists.txt' file!") 
-    (if (file-exists-p index-dir) 
-	(message "rtags_indexes is exists.") 
-      (progn 
-	(setq old-default-directory default-directory) 
-	(make-directory index-dir) 
-	(setq default-directory  index-dir) 
-	(shell-command
-	 "bash -c \"source /opt/ros/kinetic/setup.bash && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && rc -J .\"") 
-	(setq default-directory old-default-directory)))))
+	  (message "not found 'CMakeLists.txt' file!") 
+	(if (file-exists-p index-dir) 
+		(message "rtags_indexes is exists.") 
+	  (progn 
+		(setq old-default-directory default-directory) 
+		(make-directory index-dir) 
+		(setq default-directory  index-dir) 
+		(shell-command
+		 "bash -c \"source /opt/ros/kinetic/setup.bash && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && rc -J .\"") 
+		(setq default-directory old-default-directory)))))
 
 (add-hook 'c-mode-hook 'gen-rtags-indexes)
 (add-hook 'c++-mode-hook 'gen-rtags-indexes)
 (add-hook 'objc-mode-hook 'gen-rtags-indexes)
+
+;; (define-key run-mode-map (kbd "RET") '(lambda (&optional EVENT)
+;; (interactive)
+;; (delete-other-windows)
+;; (compile-goto-error EVENT)
+;; ))
+
+;; Some code that will make it so the background color of the lines
+;; that gcc found errors on, should be in another color.
+(require 'custom)
+(defvar all-overlays ())
+(defun delete-this-overlay(overlay is-after begin end &optional len) 
+  (delete-overlay overlay))
+
+(defun highlight-current-line() 
+  (interactive) 
+  (setq current-point (point)) 
+  (beginning-of-line) 
+  (setq beg (point)) 
+  (forward-line 1) 
+  (setq end (point))
+  ;; Create and place the overlay
+  (setq error-line-overlay (make-overlay 1 1))
+
+  ;; Append to list of all overlays
+  (setq all-overlays (cons error-line-overlay all-overlays))
+  (overlay-put error-line-overlay 'face '(background-color . "pink")) 
+  (overlay-put error-line-overlay 'modification-hooks (list 'delete-this-overlay)) 
+  (move-overlay error-line-overlay beg end) 
+  (goto-char current-point))
+
+(defun delete-all-overlays() 
+  (while all-overlays (delete-overlay (car all-overlays)) 
+		 (setq all-overlays (cdr all-overlays))))
+
+(defun highlight-error-lines(compilation-buffer process-result) 
+  (interactive) 
+  (delete-all-overlays) 
+  (condition-case nil (while t (next-error) 
+							 (highlight-current-line))
+	(error 
+	 nil)))
+
+;; (setq compilation-finish-function 'highlight-error-lines)
 ;;
 (provide 'mod-cc)
