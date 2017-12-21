418df5bc64d5b1c42aed197f851a549aa8ab3dad
damon-kwok <563066990@qq.com>
2017-12-21 20:44:06 +0800

    mod-cc

--- emacs-config/modules/mod-cc.el
+++ emacs-config/modules/mod-cc.el
@@ -331,7 +331,7 @@
 ;; (setq default-tab-width 4)
 ;; (setq tab-width 4)
 
-(add-hook 'c-mode-common-hook 'google-set-c-style)
+;; (add-hook 'c-mode-common-hook 'google-set-c-style)
 
 
 (defun gen-rtags-indexes ()
