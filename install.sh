90bc6e520640e2d39b0ff0977ae2c90ee19fa54f
damon-kwok <guowangwei@51hitech.com>
2018-06-08 18:09:09 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 if [ ! -e "$HOME/.oh-my-emacs/.git/index" ]; then
-	git clone https://github.com/damon-kwok/my-emacs-config $HOME/.oh-my-emacs
+	git clone https://github.com/damon-kwok/oh-my-emacs $HOME/.oh-my-emacs
 else
 	cd $HOME/.oh-my-emacs
 	git pull
 fi
 
 chmod +x $HOME/.oh-my-emacs/bin/*
-
-$HOME/.oh-my-emacs/bin/shaman install
