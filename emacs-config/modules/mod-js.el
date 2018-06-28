a6db8a802e1258f456e8cfac2211f147da109beb
damon-kwok <guowangwei@51hitech.com>
2018-06-28 20:12:28 +0800

    mod-js

--- emacs-config/modules/mod-js.el
+++ emacs-config/modules/mod-js.el
@@ -27,13 +27,13 @@
 (require 'mod-lsp)
 
 ;; `lsp-js'
-(package-require 'lsp-javascript-typescript)
-(require 'lsp-javascript-typescript)
-(add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
-(add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable) ;; for typescript support
-(add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable) ;; for js2-mode support
-(add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
-(add-hook 'rjsx-mode #'lsp-javascript-typescript-enable) ;; for rjsx-mode support
+;; (package-require 'lsp-javascript-typescript)
+;; (require 'lsp-javascript-typescript)
+;; (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
+;; (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable) ;; for typescript support
+;; (add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable) ;; for js2-mode support
+;; (add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
+;; (add-hook 'rjsx-mode #'lsp-javascript-typescript-enable) ;; for rjsx-mode support
 
 (defun my-company-transformer (candidates)
   (let ((completion-ignore-case t))
