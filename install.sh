fe36a3f72ca4393674f83bba57a4af6ef00a1dd8
damon-kwok <563066990@qq.com>
2018-07-13 01:32:12 +0800

    quick install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -e "$HOME/.oh-my-emacs/.git/index" ]; then
-	git clone https://github.com/damon-kwok/oh-my-emacs $HOME/.oh-my-emacs
+	git clone https://github.com/damon-kwok/oh-my-emacs $HOME/.oh-my-emacs --depth=1
 else
 	cd $HOME/.oh-my-emacs
 	git pull
