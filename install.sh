9cb10af870f5d4fa40bf278d52c5042cabe2c190
damon-kwok <563066990@qq.com>
2018-09-11 19:22:56 +0800

    install

--- install.sh
+++ install.sh
@@ -6,3 +6,22 @@ else
     cd $HOME/.oh-my-emacs
     git pull
 fi
+
+function link-init-el() {
+    tip "link"
+    bkdir=~/emacs-config-backup/`date +%Y-%m-%d@%H-%M-%S`
+    mkdir -p $bkdir
+
+    if [ -e ~/.emacs ]; then
+	mv ~/.emacs $bkdir
+    fi
+
+    if [ -e ~/emacs-config ]; then
+	mv ~/emacs-config $bkdir
+    fi
+
+    tip "link .emacs"
+    cp -a $OME_ROOT/emacs-config/init.el ~/.emacs 
+    ln -s $OME_ROOT/emacs-config ~/emacs-config 
+}
+link-init-el
