87c0c693acbd3477b11b60ef6b3cc0a135747657
damon-kwok <563066990@qq.com>
2018-11-17 04:53:59 +0800

    keybind

--- emacs-config/modules/mod-keybind.el
+++ emacs-config/modules/mod-keybind.el
@@ -576,17 +576,17 @@ _\\_: calendar    _<escape>_: Quit   <tab>_: <-BACK           ^ ^             ^ 
 (defhydra hydra-open-menu 
   (:color blue)
   "
-^Project^         ^Doc^          ^Blog^            ^dotfiles^   ^English^
-^^^^^^^^^^--------------------------------------------------------------------------------
-_M-1_:libknife    _h_:README.org  _i_: index.org    _1_:env     _C-b_: english-b.org
-_M-2_:medusa      _d_:diary.org   _b_: blog.org     _2_:lib     _C-2_: PETS2.org
-_M-3_:pkg-manager _t_:todo.org    _l_: link.org     _3_:tool    _C-3_: PETS3.org
-_M-4_:network     _b_:book.org    ^^                _4_:proj    _C-4_: CET4.org
-_M-5_:webscrap    _g_:game.org    ^^                _5_:ome     _C-6_: CET6.org
-^^                _n_:NEWS.org    ^^                _._:.bashrc _e_:english.org
-^^                ^^              ^^                _0_:.emacs  _v_:vocabulary.org
-^^^^^^^^^^--------------------------------------------------------------------------------
-_\\_: calendar       _<escape>_: Quit   _<tab>_: <-BACK ^^
+^Project^         ^Doc^          ^Blog^            ^dotfiles^      ^dotfiles^     ^English^
+^^^^^^^^^^^^--------------------------------------------------------------------------------
+_M-1_:libknife    _h_:README.org  _i_: index.org    _1_:env        _._:.bashrc    _C-b_: english-b.org
+_M-2_:medusa      _d_:diary.org   _b_: blog.org     _2_:lib        _0_:.emacs     _C-2_: PETS2.org
+_M-3_:pkg-manager _t_:todo.org    _l_: link.org     _3_:tool       ^^             _C-3_: PETS3.org
+_M-4_:network     _b_:book.org    ^^                _4_:proj       ^^             _C-4_: CET4.org
+_M-5_:webscrap    _g_:game.org    ^^                _5_:ome        ^^             _C-6_: CET6.org
+^^                _n_:NEWS.org    ^^                _6_:appwizard  ^^             _e_:english.org
+^^                ^^              ^^                ^^             ^^             _v_:vocabulary.org
+^^^^^^^^^^^^--------------------------------------------------------------------------------
+_\\_: calendar       _<escape>_: Quit   _<tab>_: <-BACK ^^^^
 " ;;
 
   ;; Project
@@ -615,6 +615,7 @@ _\\_: calendar       _<escape>_: Quit   _<tab>_: <-BACK ^^
   ("3" (ome-open-file "~/workspace/bin/tool") "tool") 
   ("4" (ome-open-file "~/workspace/bin/proj") "proj")
   ("5" (ome-open-file "~/workspace/bin/ome") "ome")
+  ("6" (ome-open-file "~/workspace/bin/app_wizard") "app_wizard")
   ("." (ome-open-file "~/.bashrc") ".bashrc")
   ("0" (ome-open-file "~/.emacs") ".emacs") 
   ("f" (ome-open-file "~/workspace/elfeed.org") "elfeed")
