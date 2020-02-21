a04f2d668fc2237a7faef6034df3a7a3907d732e
damon-kwok <damon-kwok@outlook.com>
2020-02-21 17:58:42 +0800

    mod-qml

---
+++ emacs-config/modules/mod-qml.el
@@ -0,0 +1,38 @@
+;; -*- lexical-binding: t -*-
+;; mod-qml.el --- This is where you apply your OCD.
+;;
+;; Copyright (C) 2009-2020 damon-kwok
+;;
+;; Author: Administrator <damon-kwok@outlook.com>
+;; Create: 2020-02-21
+;; Modify: 2020-02-21
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Code:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'mod-package)
+;;
+(package-require 'qml-mode)
+(require 'qml-mode)
+(autoload 'qml-mode "qml-mode" "Editing Qt Declarative." t)
+(add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
+
+(package-require 'company-qml)
+(require 'company-qml)
+(add-to-list 'company-backends 'company-qml)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'mod-qml)
+;; mod-qml.el ends here
