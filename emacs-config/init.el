92be07ffd504f9e6496ffd7c345eda6f480b47e7
damon-kwok <damon-kwok@outlook.com>
2020-03-06 21:11:11 +0800

    Formatting fix

--- emacs-config/init.el
+++ emacs-config/init.el
@@ -14,34 +14,28 @@
 (add-to-list 'load-path "~/.oh-my-emacs/emacs-config/preferences")
 (add-to-list 'custom-theme-load-path "~/.oh-my-emacs/emacs-config/themes")
 
-;;avoid compile error
-;;(setq byte-compile-warnings nil)
-;; (byte-recompile-directory (expand-file-name (concat (getenv "ROOT") "/.oh-emacs-config/emacs-config/modules")) 0)
-
 ;; Display the total loading time in the minibuffer
 (defun display-startup-echo-area-message () 
   "Display startup echo area message."
   (message "Initialized in %s" (emacs-init-time)))
 
-
 (require 'benchmark)
 (defun module-require (pkg) 
-  ;;(message "Module '%s' loaded in %.3fs" pkg (benchmark-elapse (require pkg))) 
-  (require pkg))
+  (message "Module '%s' loaded in %.3fs" pkg (benchmark-elapse (require pkg))) )
 
 (defun internal-require (pkg) 
-  (message "---->require '%s' in %.3fs" pkg (benchmark-elapse 
-                                              (require pkg))))
-;;(fset 'internal-require 'module-require)
-
+  ;;(message "---->require '%s' in %.3fs" pkg (benchmark-elapse (require pkg))
+  (require pkg))
+;;
 ;; Benchmark loading time file by file and display it in the *Messages* buffer
 ;; (when init-file-debug (require 'benchmark))
-
 ;; (defun module-require(mod)
 ;; (when init-file-debug
 ;; (message "Module '%s' loaded in %.2fs" mod (benchmark-elapse (require mod))))
 ;; (require mod))
-
+;;
+;; avoid compile error
+;;(setq byte-compile-warnings nil)
+;; (byte-recompile-directory (expand-file-name "~/.oh-my-emacs/emacs-config/modules") 0)
+;;
 (module-require 'mod-init)
-
-;; (toggle-frame-fullscreen)
