2e519e80036504b97903a1775bb89c12dd504a59
damon-kwok <damon-kwok@outlook.com>
2020-03-16 14:36:46 +0800

    email

--- emacs-config/modules/mod-email.el
+++ emacs-config/modules/mod-email.el
@@ -37,11 +37,50 @@
 (add-to-list 'load-path mu4e-root-linux)
 
 ;; msys2
-(setq mu4e-root-msys2 (expand-file-name (concat ome-lib-dir "/../../../../" "/usr/share/emacs/site-lisp/mu4e")))
+(setq mu4e-root-msys2 (expand-file-name (concat ome-lib-dir "/../../../../"
+                                                "/usr/share/emacs/site-lisp/mu4e")))
 (add-to-list 'load-path mu4e-root-msys2)
 
 (internal-require 'mu4e)
 (setenv "XAPIAN_CJK_NGRAM" "1")
+
+(defun mu4e~mark-get-move-target () 
+  "Ask for a move target, and propose to create it if it does not exist." 
+  (interactive)
+  ;;  (mu4e-message-at-point) ;; raises error if there is none
+  (let* ((target (mu4e-ask-maildir "Move message to: ")) 
+         (target (if (string= (substring target 0 1) "/") target (concat "/" target))) 
+         (fulltarget (expand-file-name (concat (mu4e-root-maildir) target)))) 
+    (when (or (file-directory-p fulltarget) 
+              (and (yes-or-no-p (format "1==>%s does not exist.  Create now?" fulltarget)) 
+                   (mu4e~proc-mkdir fulltarget))) target)))
+
+(defun mu4e-create-maildir-maybe (dir) 
+  "Offer to create maildir DIR if it does not exist yet.
+Return t if the dir already existed, or an attempt has been made to
+create it -- we cannot be sure creation succeeded here, since this
+is done asynchronously. Otherwise, return nil. NOte, DIR has to be
+an absolute path."
+  (let ((dir (expand-file-name dir))) 
+    (if (and (file-exists-p dir) 
+             (not (file-directory-p dir))) 
+        (mu4e-error "%s exists, but is not a directory." dir)) 
+    (cond ((file-directory-p dir) t) 
+          ((yes-or-no-p (mu4e-format "2==>%s does not exist yet. Create now?" dir)) 
+           (mu4e~proc-mkdir dir) t) 
+          (t nil))))
+
+(defun mu4e-ask-maildir-check-exists (prompt) 
+  "Like `mu4e-ask-maildir', but check for existence of the maildir,
+and offer to create it if it does not exist yet."
+  (let* ((mdir (mu4e-ask-maildir prompt)) 
+         (fullpath (expand-file-name (concat (mu4e-root-maildir) mdir)))) 
+    (unless (file-directory-p fullpath) 
+      (and (yes-or-no-p (mu4e-format "%s does not exist. Create now?" fullpath)) 
+           (mu4e~proc-mkdir fullpath))) mdir))
+
+(if (string= "Msys2" (getenv "OME_OS"))
+    (setq mu4e-doc-dir (expand-file-name (concat ome-lib-dir "/../../../../" "/usr/share/doc/mu"))))
 
 ;; `extensions'
 ;; (package-require 'mu4e-maildirs-extension)
@@ -71,9 +110,9 @@
 (internal-require 'mu4e-contrib)
 (setq mu4e-html2text-command 'mu4e-shr2text)
 
-;; (setq mu4e-drafts-folder "/Draft")
-;; (setq mu4e-sent-folder   "/Sent")
-;; (setq mu4e-trash-folder  "/Trash")
+(setq mu4e-drafts-folder "/Draft")
+(setq mu4e-sent-folder   "/Sent")
+(setq mu4e-trash-folder  "/Trash")
 ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq mu4e-sent-messages-behavior 'delete)
 
@@ -86,23 +125,23 @@
 ;; then, when you want archive some messages, move them to
 ;; the 'All Mail' folder by pressing ``ma''.
 
-;; (setq mu4e-maildir-shortcuts '( ("/INBOX" . ?i) 
-                                ;; ("/INBOX/News" . ?1) 
-                                ;; ("/INBOX/Notification" . ?2) 
-                                ;; ("/INBOX/NewMember" . ?3) 
-                                ;; ("/INBOX/JIRA" . ?4) 
-                                ;; ("/INBOX/Meeting" . ?m) 
-                                ;; ("/INBOX/MeetingSummary" . ?M) 
-                                ;; ("/INBOX/Wage" . ?w) 
-                                ;; ("/Sent" . ?s) 
-                                ;; ("/Trash" . ?t) 
-                                ;; ("/Junk" . ?j)))
+;; (setq mu4e-maildir-shortcuts '( ("/INBOX" . ?i)
+;; ("/INBOX/News" . ?1)
+;; ("/INBOX/Notification" . ?2)
+;; ("/INBOX/NewMember" . ?3)
+;; ("/INBOX/JIRA" . ?4)
+;; ("/INBOX/Meeting" . ?m)
+;; ("/INBOX/MeetingSummary" . ?M)
+;; ("/INBOX/Wage" . ?w)
+;; ("/Sent" . ?s)
+;; ("/Trash" . ?t)
+;; ("/Junk" . ?j)))
 
 (setq mu4e-index-cleanup nil   ;; don't do a full cleanup check
       mu4e-index-lazy-check t) ;; don't consider up-to-date dirs
 
 (setq mu4e-get-mail-command "mbsync -a" ;; or fetchmail, or ...
-      mu4e-update-interval 300)           ;; update every 1 minutes
+      mu4e-update-interval 300)         ;; update every 1 minutes
 
 ;; `set-play-sound'
 ;; (add-hook 'mu4e-index-updated-hook
@@ -132,11 +171,32 @@
 
 (internal-require 'smtpmail)
 ;; alternatively, for emacs-24 you can use:
-(setq message-send-mail-function 'smtpmail-send-it ;
-      smtpmail-stream-type 'ssl                    ;
-      smtpmail-default-smtp-server "smtp.office365.com"  ;
-      smtpmail-smtp-server "smtp.office365.com"          ;
+(setq message-send-mail-function 'smtpmail-send-it      ;
+      smtpmail-stream-type 'ssl                         ;
+      smtpmail-default-smtp-server "smtp.office365.com" ;
+      smtpmail-smtp-server "smtp.office365.com"         ;
       smtpmail-smtp-service 587)
+
+;; when switch off queue mode, I still prefer async sending
+;; (use-package async
+;;   :ensure t
+;;   :config (require 'smtpmail-async))
+
+;; (setq
+;;  send-mail-function 'async-smtpmail-send-it
+;;  message-send-mail-function 'async-smtpmail-send-it
+;;  ;; replace with your email provider's settings
+;;  smtpmail-smtp-server "smtp.office365.com"
+;;  smtpmail-smtp-service 587
+;;  smtpmail-stream-type 'ssl
+ 
+;;  ;; if you need offline mode, set these -- and create the queue dir
+;;  ;; with 'mu mkdir', i.e:
+;;  ;; mu mkdir /home/user/Mail/queue && touch ~/Maildir/queue/.noindex
+;;  ;; https://www.djcbsoftware.nl/code/mu/mu4e/Queuing-mail.html
+;;  smtpmail-queue-mail nil
+;;  smtpmail-queue-dir  (expand-file-name "~/Mail/Outlook/queue/cur"))
+
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
@@ -150,46 +210,46 @@
 ;;      (switch-window)))
 
 ;; `automenu'
-(defun automenu--mu4e-main-mode-menu ()
+(defun automenu--mu4e-main-mode-menu () 
   '("compose" "heads-search" "" "" "" "" "" "" "" "mark-execute"))
 
-(defun automenu--mu4e-main-mode-func (index)
+(defun automenu--mu4e-main-mode-func (index) 
   (cond ((= 0 index) 
-         (mu4e-compose-new))
-        ((= 1 index)
-         (mu4e-headers-search))
+         (mu4e-compose-new)) 
+        ((= 1 index) 
+         (mu4e-headers-search)) 
         ((= 9 index) 
          (mu4e-mark-execute-all)) 
         (t (message  "mu4e-main-mode menu:%d" index))))
 
 ;; `automenu'
-(defun automenu--mu4e-headers-mode-menu ()
+(defun automenu--mu4e-headers-mode-menu () 
   '("compose" "delete" "" "" "" "" "" "" "" "mark-execute"))
 
-(defun automenu--mu4e-headers-mode-func (index)
+(defun automenu--mu4e-headers-mode-func (index) 
   (cond ((= 0 index) 
          (mu4e-compose-new)) 
         ((= 1 index) 
-         (mu4e-compose-delete))
+         (mu4e-compose-delete)) 
         ((= 9 index) 
-         (mu4e-mark-execute-all))
+         (mu4e-mark-execute-all)) 
         (t (message  "mu4e-headers-mode menu:%d" index))))
 
 ;; `automenu'
-(defun automenu--mu4e-view-mode-menu ()
+(defun automenu--mu4e-view-mode-menu () 
   '("compose" "delete" "reply" "forward" "" "" "" "" "" "mark-execute"))
 
-(defun automenu--mu4e-view-mode-func (index)
+(defun automenu--mu4e-view-mode-func (index) 
   (cond ((= 0 index) 
          (mu4e-compose-new)) 
         ((= 1 index) 
-         (mu4e-compose-delete))
-        ((= 2 index)
-         (mu4e-compose-reply))
-        ((= 3 index)
-         (mu4e-compose-forward))
-        ((= 9 index)
-         (mu4e-mark-execute-all))
+         (mu4e-compose-delete)) 
+        ((= 2 index) 
+         (mu4e-compose-reply)) 
+        ((= 3 index) 
+         (mu4e-compose-forward)) 
+        ((= 9 index) 
+         (mu4e-mark-execute-all)) 
         (t (message  "mu4e-view-mode menu:%d" index))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'mod-email)
