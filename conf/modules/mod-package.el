7e27d92a8d2bc83408013eb7d8103bdca83426e2
Damon Kwok <damon-kwok@outlook.com>
2021-09-20 21:03:26 +0800

    Upgraded melpa mirror

--- conf/modules/mod-package.el
+++ conf/modules/mod-package.el
@@ -47,145 +47,151 @@
 ;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 (setq package-archives ;;
-  '(;;
-     ;; ("gnu" . "https://elpa.gnu.org/packages/")
-     ;; ("org" . "http://orgmode.org/elpa/")
-     ;; ("melpa" . "https://melpa.org/packages/")
-     ;;
-     ;;("gnu-china" . "http://elpa.emacs-china.org/gnu/")
-     ;;("melpa-china" . "http://elpa.emacs-china.org/melpa/")
-     ;;("org-china" . "http://elpa.emacs-china.org/org/")
-     ;;
-     ;;'("popkit" . "http://elpa.popkit.org/packages/")
-     ;;
-     ("gnu-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-     ("melpa-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-     ("org-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-     ;;
-     ))
+      '(;;
+        ;; ("gnu" . "https://elpa.gnu.org/packages/")
+        ;; ("org" . "http://orgmode.org/elpa/")
+        ;; ("melpa" . "https://melpa.org/packages/")
+        ;;
+        ;; ("gnu-china" . "http://elpa.emacs-china.org/gnu/")
+        ;; ("melpa-china" . "http://elpa.emacs-china.org/melpa/")
+        ;; ("melpa-stable-china" . "http://elpa.emacs-china.org/stable-melpa/")
+        ;; ("org-china" . "http://elpa.emacs-china.org/org/")
+        ;;
+        ;;'("popkit" . "http://elpa.popkit.org/packages/")
+        ;;
+        ("gnu-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/") 
+        ("melpa-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") 
+        ("org-tuna" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+        ;;
+        ("gnu-ustc" . "http://mirrors.ustc.edu.cn/elpa/gnu/") 
+        ("melpa-ustc" . "http://mirrors.ustc.edu.cn/elpa/melpa/") 
+        ;; ("melpa-stable-ustc" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/") 
+        ("org-ustc" . "http://mirrors.ustc.edu.cn/elpa/org/")
+        ;;
+        ))
 
 ;;(add-to-list 'load-path "~/conf/elpa-mirror")
 ;;(internal-require 'elpa-mirror)
 
-(defun online? ()
-  (if (and (functionp 'network-interface-list)
-        (network-interface-list))
-    (some (lambda (iface)
-            (unless (equal "lo" (car iface))
-              (member 'up (first (last (network-interface-info (car iface)))))))
-      (network-interface-list)) t))
+(defun online? () 
+  (if (and (functionp 'network-interface-list) 
+           (network-interface-list)) 
+      (some (lambda (iface) 
+              (unless (equal "lo" (car iface)) 
+                (member 'up (first (last (network-interface-info (car
+                                                                  iface))))))) 
+            (network-interface-list)) t))
 
 ;;(when (online?)
 ;;  (unless package-archive-contents (package-refresh-contents)))
 
-(defun lazy-require (ext mode)
+(defun lazy-require (ext mode) 
   (add-hook 'find-file-hook             ;
-    `(lambda ()
-       (when (and (stringp buffer-file-name)
-               (string-match (concat "\\." ,ext "\\'") buffer-file-name))
-         (internal-require (quote ,mode))
-         (,mode)))))
+            `(lambda () 
+               (when (and (stringp buffer-file-name) 
+                          (string-match (concat "\\." ,ext "\\'")
+                                        buffer-file-name)) 
+                 (internal-require (quote ,mode)) 
+                 (,mode)))))
 
 ;; (lazy-require "soy" 'soy-mode)
 ;; (lazy-require "tpl" 'tpl-mode)
 
 ;; To get the package manager going, we invoke its initialise function.
-(defun package-download (pkg)
-  (when (not (package-installed-p pkg))
-    (progn (unless package-archive-contents (package-refresh-contents))
-      (package-install pkg))))
+(defun package-download (pkg) 
+  (when (not (package-installed-p pkg)) 
+    (progn (unless package-archive-contents (package-refresh-contents)) 
+           (package-install pkg))))
 
-(defun package-require(pkg &optional alias-pkg alias-pkg2)
-  (package-download pkg)
-  (if alias-pkg (internal-require alias-pkg)
-    (internal-require pkg))
+(defun package-require(pkg &optional alias-pkg alias-pkg2) 
+  (package-download pkg) 
+  (if alias-pkg (internal-require alias-pkg) 
+    (internal-require pkg)) 
   (if alias-pkg2 (internal-require alias-pkg2)))
 
-(defun package-download-git(lib-name repo)
-  (let* ((oldir default-directory)
-          (ome-lib-dir-git (concat ome-lib-dir "/git"))
-          (dir-name (concat ome-lib-dir-git "/" (file-name-base lib-name)))
-          (cmd-update (concat "git pull"))
-          (cmd-clone (concat "git clone " repo " --depth=1 " lib-name)))
-    (make-directory ome-lib-dir-git t)
-    (add-to-list 'load-path dir-name)
+(defun package-download-git(lib-name repo) 
+  (let* ((oldir default-directory) 
+         (ome-lib-dir-git (concat ome-lib-dir "/git")) 
+         (dir-name (concat ome-lib-dir-git "/" (file-name-base lib-name))) 
+         (cmd-update (concat "git pull")) 
+         (cmd-clone (concat "git clone " repo " --depth=1 " lib-name))) 
+    (make-directory ome-lib-dir-git t) 
+    (add-to-list 'load-path dir-name) 
     (message "dir-name:%s" dir-name)
-
-    (if (file-exists-p dir-name)
-      (progn
-        ;; (setq default-directory dir-name)
-        ;; (ome-run-command cmd-update)
-        )
-      (progn
-        (setq default-directory ome-lib-dir-git)
-        (ome-run-command cmd-clone)))
+    (if (file-exists-p dir-name) 
+        (progn
+          ;; (setq default-directory dir-name)
+          ;; (ome-run-command cmd-update)
+          ) 
+      (progn 
+        (setq default-directory ome-lib-dir-git) 
+        (ome-run-command cmd-clone))) 
     (setq default-directory oldir)))
 
-(defun package-require-git(dir-name file-name url)
-  (let* ((pkg (intern dir-name)))
-    (package-download-git dir-name)
+(defun package-require-git(dir-name file-name url) 
+  (let* ((pkg (intern dir-name))) 
+    (package-download-git dir-name) 
     (internal-require pkg)))
 
-(defun package-download-svn(lib-name path)
-  (let* ((oldir default-directory)
-          (ome-lib-dir-svn (concat ome-lib-dir "/svn"))
-          (dir-name (concat ome-lib-dir-svn "/" (file-name-base lib-name)))
-          (cmd-update (concat "svn up"))
-          (cmd-clone (concat "svn co " path lib-name)))
-    (make-directory ome-lib-dir-svn t)
+(defun package-download-svn(lib-name path) 
+  (let* ((oldir default-directory) 
+         (ome-lib-dir-svn (concat ome-lib-dir "/svn")) 
+         (dir-name (concat ome-lib-dir-svn "/" (file-name-base lib-name))) 
+         (cmd-update (concat "svn up")) 
+         (cmd-clone (concat "svn co " path lib-name))) 
+    (make-directory ome-lib-dir-svn t) 
     (add-to-list 'load-path dir-name)
-
-    (if (file-exists-p dir-name)
-      (progn
-        ;; (setq default-directory dir-name)
-        ;; (call-process-shell-command cmd-update nil t)
-        )
-      (progn
-        (setq default-directory ome-lib-dir-svn)
-        (call-process-shell-command cmd-clone nil nil t)))
+    (if (file-exists-p dir-name) 
+        (progn
+          ;; (setq default-directory dir-name)
+          ;; (call-process-shell-command cmd-update nil t)
+          ) 
+      (progn 
+        (setq default-directory ome-lib-dir-svn) 
+        (call-process-shell-command cmd-clone nil nil t))) 
     (setq default-directory oldir)))
 
-(defun package-require-svn(dir-name file-name url)
-  (let* ((pkg (intern dirname)))
-    (package-download-svn dir-name)
+(defun package-require-svn(dir-name file-name url) 
+  (let* ((pkg (intern dirname))) 
+    (package-download-svn dir-name) 
     (internal-require pkg)))
 
-(defun package-download-curl(dir-name file-name url)
-  (let* ((oldir default-directory)
-          (dir (concat ome-lib-dir "/curl/" dir-name))
-          (full-name (concat dir "/" file-name))
-          (cmd (concat "curl -o " full-name " " url)))
-    (make-directory dir t)
-    (add-to-list 'load-path dir)
-    (setq default-directory dir-name)
-    (unless (file-exists-p full-name)
-      (progn
-        (setq default-directory dir)
-        (call-process-shell-command cmd nil nil t)))
+(defun package-download-curl(dir-name file-name url) 
+  (let* ((oldir default-directory) 
+         (dir (concat ome-lib-dir "/curl/" dir-name)) 
+         (full-name (concat dir "/" file-name)) 
+         (cmd (concat "curl -o " full-name " " url))) 
+    (make-directory dir t) 
+    (add-to-list 'load-path dir) 
+    (setq default-directory dir-name) 
+    (unless (file-exists-p full-name) 
+      (progn 
+        (setq default-directory dir) 
+        (call-process-shell-command cmd nil nil t))) 
     (setq default-directory oldir)))
 
-(defun package-require-curl(dir-name file-name url)
-  (let* ((pkg (intern dir-name)))
-    (package-download-curl dir-name file-name url)
+(defun package-require-curl(dir-name file-name url) 
+  (let* ((pkg (intern dir-name))) 
+    (package-download-curl dir-name file-name url) 
     (internal-require pkg)))
 
-(defun package-update()
-  (interactive)
-  (save-window-excursion (package-list-packages t)
-    (package-refresh-contents)))
+(defun package-update() 
+  (interactive) 
+  (save-window-excursion (package-list-packages t) 
+                         (package-refresh-contents)))
 
-(defun package-upgrade()
-  "Upgrade installed"
-  (interactive)
-  (save-window-excursion (package-list-packages t)
-    (package-menu-mark-upgrades)
-    (package-menu-mark-obsolete-for-deletion)
-    (package-menu-execute t)))
+(defun package-upgrade() 
+  "Upgrade installed" 
+  (interactive) 
+  (save-window-excursion (package-list-packages t) 
+                         (package-menu-mark-upgrades) 
+                         (package-menu-mark-obsolete-for-deletion) 
+                         (package-menu-execute t)))
 
-(defun package-dist-upgrade()
-  (interactive)
-  (package-update)
-  (package-upgrade)
+(defun package-dist-upgrade() 
+  (interactive) 
+  (package-update) 
+  (package-upgrade) 
   (package-autoremove))
 
 ;; `use-package'
@@ -197,19 +203,21 @@
 ;;
 ;; (auto-install-from-url "https://raw.github.com/aki2o/guide-key-tip/master/guide-key-tip.el")
 
-(package-download-curl "xcowsay" "xcowsay.el" "https://www.emacswiki.org/emacs/download/xcowsay.el")
+(package-download-curl "xcowsay" "xcowsay.el"
+                       "https://www.emacswiki.org/emacs/download/xcowsay.el")
 
 (package-download-curl "pink-bliss" "pink-bliss-theme.el"
-  "https://raw.githubusercontent.com/kensanata/elisp/master/pink-bliss-theme.el")
+                       "https://raw.githubusercontent.com/kensanata/elisp/master/pink-bliss-theme.el")
 (package-download-curl "pink-bliss" "pink-bliss.el"
-  "https://www.emacswiki.org/emacs/download/pink-bliss.el")
+                       "https://www.emacswiki.org/emacs/download/pink-bliss.el")
 (package-download-curl "pink-bliss" "pink-gnu.xpm"
-  "http://www.emacswiki.org/emacs/download/pink-gnu.xpm")
+                       "http://www.emacswiki.org/emacs/download/pink-gnu.xpm")
 
-(package-download-curl "visws" "visws.el" "https://www.emacswiki.org/emacs/download/visws.el")
+(package-download-curl "visws" "visws.el"
+                       "https://www.emacswiki.org/emacs/download/visws.el")
 
 (package-download-curl "multi-term" "multi-term.el"
-  "https://www.emacswiki.org/emacs/download/multi-term.el")
+                       "https://www.emacswiki.org/emacs/download/multi-term.el")
 (setq multi-term-program "/bin/bash")
 
 (package-require 'package-lint)
