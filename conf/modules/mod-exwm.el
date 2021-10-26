8a714f553a1bc9866009cbb9e5943536b28f0849
Damon Kwok <damon-kwok@outlook.com>
2021-10-26 16:48:02 +0800

    exwm: kybind

--- conf/modules/mod-exwm.el
+++ conf/modules/mod-exwm.el
@@ -25,71 +25,71 @@
 
 (package-require 'exwm)
 (package-require 'exwm-x)
-;; (package-require 'helm-exwm)
-;; (package-download 'exwm-firefox-core)
+(package-require 'helm-exwm)
 (require 'exwm)
 (require 'exwm-x)
-(require 'exwm-systemtray)
-(setq exwm-systemtray-height 30)
-(exwm-systemtray-enable)
-;; (setq exwm-input-global-keys `(,(kbd "s-&") .
-                               ;; (lambda (command)
-                                 ;; (interactive (list (read-shell-command "$ ")))
-                                 ;; (start-process-shell-command command nil command))))
-;; (eval-after-load 'exwm (require 'exwm-firefox-core))
-;; (setq exwmx-button-floating-button-line 'mode-line)
-
-(require 'ido)
-
-(define-obsolete-function-alias 'exwm-config-default
-  #'exwm-config-ome "27.1")
 (require 'exwm-config)
+(require 'exwm-systemtray)
 
+;; (package-download 'exwm-firefox-core)
+;; (eval-after-load 'exwm (require 'exwm-firefox-core))
 
-(defun exwm-config-ome ()
+(define-obsolete-function-alias 'exwm-config-default #'exwm-config-ome "27.1")
+
+(setq exwm-systemtray-height 20)
+(exwm-systemtray-enable)
+
+(defun exwm-config-ome () 
   "Default configuration of EXWM."
   ;; Set the initial workspace number.
-  (unless (get 'exwm-workspace-number 'saved-value)
+  (unless (get 'exwm-workspace-number 'saved-value) 
     (setq exwm-workspace-number 4))
   ;; Make class name the buffer name
-  (add-hook 'exwm-update-class-hook
-            (lambda ()
-              (exwm-workspace-rename-buffer exwm-class-name)))
+  (add-hook 'exwm-update-class-hook (lambda () 
+                                      (exwm-workspace-rename-buffer
+                                       exwm-class-name)))
   ;; Global keybindings.
-  (unless (get 'exwm-input-global-keys 'saved-value)
-    (setq exwm-input-global-keys
-          `(
-                        ([?\s-x] . run-nemo)
-            ([?\s-z] . run-firefox)
-            ([?\s-d] . exwmx-dmenu)
-            ([?\s-t] . run-terminal)
+  ;; 's-p': Launch application
+  (exwm-input-set-key (kbd "s-p") 'helm-run-external-command)
+  ;; 's-RET': Launch terminal
+  (exwm-input-set-key (kbd "s-<return>") #'run-terminal) 
+  (unless (get 'exwm-input-global-keys 'saved-value) 
+    (setq exwm-input-global-keys ;;
+          `(([?\s-x] . run-nemo) 
+            ([?\s-z] . run-firefox) 
+            ([?\s-t] . run-terminal) 
+            ([?\s-d] . helm-run-external-command)
+
             ;; 's-r': Reset (to line-mode).
             ([?\s-r] . exwm-reset)
+
             ;; 's-w': Switch workspace.
             ([?\s-w] . exwm-workspace-switch)
+
             ;; 's-&': Launch application.
-            ([?\s-&] . (lambda (command)
-                         (interactive (list (read-shell-command "$ ")))
+            ([?\s-&] . (lambda (command) 
+                         (interactive (list (read-shell-command "$ "))) 
                          (start-process-shell-command command nil command)))
+
             ;; 's-N': Switch to certain workspace.
-            ,@(mapcar (lambda (i)
-                        `(,(kbd (format "s-%d" i)) .
-                          (lambda ()
-                            (interactive)
-                            (exwm-workspace-switch-create ,i))))
+            ,@(mapcar (lambda (i)                    ;;
+                        `(,(kbd (format "s-%d" i)) . ;;
+                          (lambda () 
+                            (interactive) 
+                            (exwm-workspace-switch-create ,i)))) 
                       (number-sequence 0 9)))))
   ;; Line-editing shortcuts
-  (unless (get 'exwm-input-simulation-keys 'saved-value)
-    (setq exwm-input-simulation-keys
-          '(([?\C-b] . [left])
-            ([?\C-f] . [right])
-            ([?\C-p] . [up])
-            ([?\C-n] . [down])
-            ([?\C-a] . [home])
-            ([?\C-e] . [end])
-            ([?\M-v] . [prior])
-            ([?\C-v] . [next])
-            ([?\C-d] . [delete])
+  (unless (get 'exwm-input-simulation-keys 'saved-value) 
+    (setq exwm-input-simulation-keys ;;
+          '(([?\C-b] . [left]) 
+            ([?\C-f] . [right]) 
+            ([?\C-p] . [up]) 
+            ([?\C-n] . [down]) 
+            ([?\C-a] . [home]) 
+            ([?\C-e] . [end]) 
+            ([?\M-v] . [prior]) 
+            ([?\C-v] . [next]) 
+            ([?\C-d] . [delete]) 
             ([?\C-k] . [S-end delete]))))
   ;; Enable EXWM
   (exwm-enable)
@@ -98,97 +98,20 @@
   ;; Other configurations
   (exwm-config-misc))
 
-(defun exwm-config--fix/ido-buffer-window-other-frame ()
-  "Fix `ido-buffer-window-other-frame'."
-  (defalias 'exwm-config-ido-buffer-window-other-frame
-    (symbol-function #'ido-buffer-window-other-frame))
-  (defun ido-buffer-window-other-frame (buffer)
-    "This is a version redefined by EXWM.
-
-You can find the original one at `exwm-config-ido-buffer-window-other-frame'."
-    (with-current-buffer (window-buffer (selected-window))
-      (if (and (derived-mode-p 'exwm-mode)
-               exwm--floating-frame)
-          ;; Switch from a floating frame.
-          (with-current-buffer buffer
-            (if (and (derived-mode-p 'exwm-mode)
-                     exwm--floating-frame
-                     (eq exwm--frame exwm-workspace--current))
-                ;; Switch to another floating frame.
-                (frame-root-window exwm--floating-frame)
-              ;; Do not switch if the buffer is not on the current workspace.
-              (or (get-buffer-window buffer exwm-workspace--current)
-                  (selected-window))))
-        (with-current-buffer buffer
-          (when (derived-mode-p 'exwm-mode)
-            (if (eq exwm--frame exwm-workspace--current)
-                (when exwm--floating-frame
-                  ;; Switch to a floating frame on the current workspace.
-                  (frame-selected-window exwm--floating-frame))
-              ;; Do not switch to exwm-mode buffers on other workspace (which
-              ;; won't work unless `exwm-layout-show-all-buffers' is set)
-              (unless exwm-layout-show-all-buffers
-                (selected-window)))))))))
-
-(defun exwm-config--fix/ido-buffer-window-other-frame ()
-  "Fix `ido-buffer-window-other-frame'."
-  (defalias 'exwm-config-ido-buffer-window-other-frame
-    (symbol-function #'ido-buffer-window-other-frame))
-  (defun ido-buffer-window-other-frame (buffer)
-    "This is a version redefined by EXWM.
-
-You can find the original one at `exwm-config-ido-buffer-window-other-frame'."
-    (with-current-buffer (window-buffer (selected-window))
-      (if (and (derived-mode-p 'exwm-mode)
-               exwm--floating-frame)
-          ;; Switch from a floating frame.
-          (with-current-buffer buffer
-            (if (and (derived-mode-p 'exwm-mode)
-                     exwm--floating-frame
-                     (eq exwm--frame exwm-workspace--current))
-                ;; Switch to another floating frame.
-                (frame-root-window exwm--floating-frame)
-              ;; Do not switch if the buffer is not on the current workspace.
-              (or (get-buffer-window buffer exwm-workspace--current)
-                  (selected-window))))
-        (with-current-buffer buffer
-          (when (derived-mode-p 'exwm-mode)
-            (if (eq exwm--frame exwm-workspace--current)
-                (when exwm--floating-frame
-                  ;; Switch to a floating frame on the current workspace.
-                  (frame-selected-window exwm--floating-frame))
-              ;; Do not switch to exwm-mode buffers on other workspace (which
-              ;; won't work unless `exwm-layout-show-all-buffers' is set)
-              (unless exwm-layout-show-all-buffers
-                (selected-window)))))))))
-
-(defun exwm-config-ido ()
-  "Configure Ido to work with EXWM."
-  (ido-mode 1)
-  (add-hook 'exwm-init-hook #'exwm-config--fix/ido-buffer-window-other-frame))
-
-(defun exwm-config-misc ()
-  "Other configurations."
-  ;; Make more room
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (fringe-mode 1))
-
-(defun run-nemo ()
-  (interactive)
+(defun run-nemo () 
+  (interactive) 
   (exwmx-quickrun "nemo"))
 
-(defun run-firefox ()
-  (interactive)
+(defun run-firefox () 
+  (interactive) 
   (exwmx-quickrun "firefox"))
 
 ;; (defun run-rofi ()
-  ;; (interactive)
-  ;; (exwmx-quickrun "rofi -show run"))
+;; (interactive)
+;; (exwmx-quickrun "rofi -show run"))
 
-(defun run-terminal ()
-  (interactive)
+(defun run-terminal () 
+  (interactive) 
   (exwmx-quickrun "gnome-terminal"))
 
 (exwm-config-ome)
@@ -196,4 +119,3 @@ You can find the original one at `exwm-config-ido-buffer-window-other-frame'."
 (provide 'mod-exwm)
 
 ;;; mod-exwm.el ends here
-
